d75dd9bd429edd18fc2db45b2ad0051e357b39ee
Arnis Vuskans <ampatspell@gmail.com>
2025-10-22 22:04:46 +0300

    fix gpio

--- electron/gpio.ts
+++ electron/gpio.ts
@@ -1,8 +1,8 @@
 import { Gpio } from "onoff";
 
 export const startGPIO = async () => {
-  const ledpin = new Gpio(26, 'out');
-  const pushpin = new Gpio(4, 'in', 'both');
+  const ledpin = new Gpio(538, 'out');
+  const pushpin = new Gpio(516, 'in', 'both');
 
   pushpin.watch((err, value) => {
     console.log(err, value, value?.toFixed(2));
