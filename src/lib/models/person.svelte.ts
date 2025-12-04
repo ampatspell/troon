d04fc69d49be644070c34e9a0e3d2aa00e4c8e71
Arnis Vuskans <ampatspell@gmail.com>
2025-12-04 15:46:31 +0200

    Update person.svelte.ts

--- src/lib/models/person.svelte.ts
+++ src/lib/models/person.svelte.ts
@@ -1,19 +1,47 @@
 import { Sound } from './sound.svelte';
 import { rnd } from './utils';
 
+class Played {
+  played: number[] = [];
+
+  add(value: number) {
+    const played = this.played;
+    if(played.length > 5) {
+      played.shift();
+    }
+    played.push(value);
+  }
+
+  try(cb: () => number): number {
+    for(;;) {
+      const next = cb();
+      if(!this.has(next)) {
+        this.add(next);
+        return next;
+      }
+    }
+  }
+
+  has(value: number) {
+    return this.played.includes(value);
+  }
+}
+
 export class Person {
   readonly sounds: Sound[];
+  readonly played: Played;
 
   constructor(
     public readonly name: string,
     sounds: string[],
   ) {
     this.sounds = sounds.map((name) => new Sound(name));
+    this.played = new Played();
   }
 
   speak() {
     const sounds = this.sounds;
-    const idx = rnd(0, sounds.length - 1);
+    const idx = this.played.try(() => rnd(0, sounds.length - 1));
     const sound = sounds[idx];
     sound.play();
   }
